41d75207b2dcc007e9dcee9bc949a43bc9428fa8
davidricardo1026 <david.lee.ricardo1026@gmail.com>
2024-07-07 22:50:30 +0800

    update

--- simpleflow-core-impl/src/main/java/org/ss/simpleflow/core/impl/validate/SfDefaultValidateManager.java
+++ simpleflow-core-impl/src/main/java/org/ss/simpleflow/core/impl/validate/SfDefaultValidateManager.java
@@ -1,18 +1,23 @@
 package org.ss.simpleflow.core.impl.validate;
 
+import org.ss.simpleflow.common.CollectionUtils;
 import org.ss.simpleflow.core.line.SfAbstractLineConfig;
 import org.ss.simpleflow.core.node.SfAbstractNodeConfig;
+import org.ss.simpleflow.core.processconfig.SfProcessConfig;
 import org.ss.simpleflow.core.processconfig.SfProcessConfigGraph;
 import org.ss.simpleflow.core.processengine.SfProcessEngineConfig;
 import org.ss.simpleflow.core.validate.SfLineConfigCustomValidator;
 import org.ss.simpleflow.core.validate.SfNodeConfigCustomValidator;
 import org.ss.simpleflow.core.validate.SfValidateManager;
 
+import java.util.List;
+
 public class SfDefaultValidateManager<NODE_ID, LINE_ID, PROCESS_CONFIG_ID,
         NODE_CONFIG extends SfAbstractNodeConfig<NODE_ID, PROCESS_CONFIG_ID>,
         LINE_CONFIG extends SfAbstractLineConfig<LINE_ID, NODE_ID>,
-        PROCESS_CONFIG_GRAPH extends SfProcessConfigGraph<NODE_ID, LINE_ID, PROCESS_CONFIG_ID, NODE_CONFIG, LINE_CONFIG>>
-        implements SfValidateManager<NODE_ID, LINE_ID, PROCESS_CONFIG_ID, NODE_CONFIG, LINE_CONFIG, PROCESS_CONFIG_GRAPH> {
+        PROCESS_CONFIG_GRAPH extends SfProcessConfigGraph<NODE_ID, LINE_ID, PROCESS_CONFIG_ID, NODE_CONFIG, LINE_CONFIG>,
+        PROCESS_CONFIG extends SfProcessConfig<NODE_ID, LINE_ID, PROCESS_CONFIG_ID, NODE_CONFIG, LINE_CONFIG, PROCESS_CONFIG_GRAPH>>
+        implements SfValidateManager<NODE_ID, LINE_ID, PROCESS_CONFIG_ID, NODE_CONFIG, LINE_CONFIG, PROCESS_CONFIG_GRAPH, PROCESS_CONFIG> {
 
     private final SfDefaultBasicValidator<NODE_ID, LINE_ID, PROCESS_CONFIG_ID, NODE_CONFIG, LINE_CONFIG, PROCESS_CONFIG_GRAPH> basicValidator;
 
@@ -21,10 +26,28 @@ public class SfDefaultValidateManager<NODE_ID, LINE_ID, PROCESS_CONFIG_ID,
         basicValidator = new SfDefaultBasicValidator<>(nodeConfigCustomValidator, lineConfigCustomValidator);
     }
 
-
     @Override
-    public void manageValidate(PROCESS_CONFIG_GRAPH processConfig, SfProcessEngineConfig processEngineConfig) {
+    public void validate(PROCESS_CONFIG processConfig, SfProcessEngineConfig processEngineConfig) {
+        List<NODE_CONFIG> nodeConfigList = processConfig.getNodeConfigList();
+        List<LINE_CONFIG> lineConfigList = processConfig.getLineConfigList();
 
+        validateProcess(nodeConfigList, lineConfigList, processEngineConfig);
+        List<PROCESS_CONFIG_GRAPH> subProcessConfigList = processConfig.getSubProcessConfigList();
+
+        if (CollectionUtils.isNotEmpty(subProcessConfigList)) {
+            for (PROCESS_CONFIG_GRAPH processConfigGraph : subProcessConfigList) {
+                List<NODE_CONFIG> subProcessNodeConfigList = processConfigGraph.getNodeConfigList();
+                List<LINE_CONFIG> subProcessLineConfigList = processConfigGraph.getLineConfigList();
+                validateProcess(subProcessNodeConfigList, subProcessLineConfigList, processEngineConfig);
+            }
+        }
     }
 
+    private void validateProcess(List<NODE_CONFIG> nodeConfigList,
+                                 List<LINE_CONFIG> lineConfigList,
+                                 SfProcessEngineConfig processEngineConfig) {
+        if (CollectionUtils.isNullOrEmpty(nodeConfigList)) {
+
+        }
+    }
 }
